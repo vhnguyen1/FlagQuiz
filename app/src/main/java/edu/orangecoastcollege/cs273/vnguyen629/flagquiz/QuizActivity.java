945c8fc242e32e22942e7e08bd810a5be3c80d09
vnguyen629 <brotherflab@gmail.com>
2016-09-22 16:14:55 -0700

    Added landscape, editing all activites

--- app/src/main/java/edu/orangecoastcollege/cs273/vnguyen629/flagquiz/QuizActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/vnguyen629/flagquiz/QuizActivity.java
@@ -1,22 +1,47 @@
 package edu.orangecoastcollege.cs273.vnguyen629.flagquiz;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
+/**
+ *
+ */
 public class QuizActivity extends AppCompatActivity {
+    // keys for reading data from SharedPreferences
+    public static final String CHOICES = "pref_numberOfChoices";
+    public static final String REGIONS = "pref_regionsToInclude";
 
+    private boolean phoneDevice = true; // used to force portrait mode
+    private boolean preferencesChanged = true; // did preferences change?
+
+    /**
+     * onCreate generates the appropriate layout to inflate, depending on the
+     * screen size. IF the device is large or x-large, it will load the content_main.xml
+     * (sw700dp-land) which includes both the fragment_quiz.xml and fragment_settings.xml.
+     * Otherwise, it just inflates the standard content_main.xml with the fragment quiz.
+     *
+     * All default preferences are set using the preferences.xml file.
+     * @param savedInstanceState The saved state to restore (not being used)
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // set default values in the app's SharedPreferences
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
+        // register listener for SharedPreferences
+        
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/edu/orangecoastcollege/cs273/vnguyen629/flagquiz/SettingsActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/vnguyen629/flagquiz/SettingsActivity.java
@@ -7,6 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+/**
+ *
+ */
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
